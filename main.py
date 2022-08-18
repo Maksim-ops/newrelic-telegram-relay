f1b8f5acbe3fcd364d4e475bb809a6e424532607
tarikly <tarikly+github@gmail.com>
2022-08-18 02:04:49 -0300

    fix heroku Procfile

--- main.py
+++ main.py
@@ -42,5 +42,5 @@ api.add_resource(WebHook, '/webhook')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('APP_PORT', 5000))
     app.run(host='0.0.0.0', port=port)
