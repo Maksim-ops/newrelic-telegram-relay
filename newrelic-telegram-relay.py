3e78deb16f286226f9860cb301fc90028d8c1f82
tarikly <tarikly+github@gmail.com>
2022-08-18 01:35:36 -0300

    fix heroku Procfile

--- newrelic-telegram-relay.py
+++ newrelic-telegram-relay.py
@@ -40,4 +40,6 @@ class WebHook(Resource):
 api.add_resource(WebHook, '/webhook')
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8080, host='0.0.0.0')
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
