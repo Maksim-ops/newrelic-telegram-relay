0495b7b039bebacfa30d069373945b6a86f00a1c
Maksim <72493054+Maksim-ops@users.noreply.github.com>
2022-09-07 09:08:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,43 +7,29 @@ import os
 app = Flask(__name__)
 api = Api(app)
 
-
 with open('config/telegram.json') as f:
     telegram_config = json.load(f)
 
-token = telegram_config['botToken']
+bot_token = telegram_config['botToken']
 chat_id = telegram_config['chatId']
-
 
 class WebHook(Resource):
     def post(self):
         payload = request.json
         print('Received webhook.')
         print(payload)
+        if hasattr(payload, 'bot_token'):
+            bot_token = payload.get('bot_token')
+        if hasattr(payload, 'chat_id'):
+            chat_id = payload.get('chat_id')
         print('Sending message to telegram chat: ' + chat_id)
-        state = payload.get('current_state')
-        if state == 'open':
-            state_title = '🚨 ' + \
-                payload.get('event_type') + ' ' + state
-        else:
-            state_title = '✅ ' + \
-                payload.get('event_type') + ' ' + state
-        message = "*" + state_title + '*\n' \
-            '*Policy*: ' + payload.get('policy_name') + '\n' \
-            '*Details*: ' + payload.get('details') + '\n' \
-            '*Time*: ' + payload.get('timestamp_utc_string') + '\n' \
-            '[Chart](' + payload.get('violation_chart_url') + ')'
         try:
-            r = send_telegram_message(token, chat_id, message)
-            return r
+            res = send_telegram_message(bot_token, chat_id, json.dumps(payload))
+            return res
         except Exception as e:
             return e
-        return 'OK'
-
 
 api.add_resource(WebHook, '/webhook')
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('APP_PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
